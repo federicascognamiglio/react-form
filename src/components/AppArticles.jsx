fc7ddc5a7882c05ce0d5f516fc3e63951870a22b
federicascognamiglio <scognamigliofede99@gmail.com>
2024-12-17 16:36:14 +0100

    feat: submit form completed

--- src/components/AppArticles.jsx
+++ src/components/AppArticles.jsx
@@ -2,31 +2,41 @@ import { useState } from 'react'
 import articles from '../data/articles';
 
 function AppArticles() {
-    const [title, setTitle] = useState("");
+    // Variabili
+    const [newTitle, setTitle] = useState("");
     const [articlesList, setArticlesList] = useState(articles);
+
+    // Submit Function
     const handleSubmit = (event) => {
         event.preventDefault()
-        const newList = [...articlesList, title];
-        setArticlesList(newList);
+        const newArticle = {
+            id: Date.now(),
+            title: newTitle 
+        }
+        setArticlesList([...articlesList, newArticle]);
         setTitle("");
     }
 
     return (
         <>
+            {/* Form */}
             <form onSubmit={handleSubmit} className='mt-4'>
                 <div className='w-50'>
                     <label htmlFor="articleTitle" className='form-label'>Article Title</label>
-                    <input className='form-control' value={title} onChange={(event) => setTitle(event.target.value)} type="text" id='articleTitle' />
+                    <input className='form-control' value={newTitle} onChange={(event) => setTitle(event.target.value)} type="text" id='articleTitle' />
                 </div>
                 <button type='submit' className="btn btn-primary mt-3">Salva</button>
             </form>
+
+            {/* Articles */}
             <section className='mt-5'>
                 <h3 className='pb-3'>My Articles</h3>
                 {articlesList.map(curArtcile =>
                     <div key={curArtcile.id} className="mb-3">
                         <div className="card">
-                            <div className="card-body">
+                            <div className="card-body d-flex justify-content-between align-items-center">
                                 <h5 className="card-title">{curArtcile.title}</h5>
+                                <button className='btn btn-outline-danger'>Delete</button>
                             </div>
                         </div>
                     </div>)}
